08aaf24befde6f6d0b4a0d328c8c7a9e2a50aa5d
Fenfei Guo <guofenfei1991@gmail.com>
2016-12-03 14:44:02 -0700

    simple demo

---
+++ interface/web_test.py
@@ -0,0 +1,34 @@
+import tornado.ioloop
+import tornado.web
+
+from trainer import Trainer
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.html")
+
+class DataHandler(tornado.web.RequestHandler):
+    def initialize(self, trainer):
+        self.trainer = trainer
+
+    def get(self):
+        self.write(self.trainer.get_data())
+
+class UserHandler(tornado.web.RequestHandler):
+    def post(self):
+        data = self.get_argument('query')
+        print(data)
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+        (r"/data", DataHandler, {'trainer': Trainer()}),
+        (r"/user", UserHandler)
+    ])
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
+
